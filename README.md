cb95be6f10d53e8311d3582c19aaee10a1fbf521
redbeardmcg <rmcguire@libretechconsulting.com>
2021-10-14 11:59:24 -0400

    Initial check-in

---
+++ README.md
@@ -0,0 +1,76 @@
+# Client/Server script to check firewall ports
+### Author: Ryan McGuire
+
+## Config
+In either server or client mode, a config file is specified containing 
+server and client config entries. The yaml config can contain both, or only
+one or the other.
+
+A sample server definition looks like this (listening address is optional):
+```
+server:
+  ports:
+    - 0.0.0.0:22
+    - 5432
+    - 443
+    - 8443
+```
+
+A sample client definition looks like this:
+```
+client:
+  targets:
+    127.0.0.1:
+      ports:
+        - 22
+        - 5432
+        - 443
+        - 8443
+    192.168.0.1:
+      ports:
+        - 22
+        - 443
+```
+
+## Running
+A python3 and python2 version are both provided.
+
+To run in server mode:
+```
+check_fw.py -f <some_file.yaml> -v -s
+```
+
+To run in client mode:
+```
+check_fw.py -f <some_file.yaml> -v -c
+```
+
+## Output (Server)
+** Found server declaration, running ports...
+        ** Port declarations found, ensuring listeners on ports:
+                ** 192.168.0.181:22
+                        ** Port ('192.168.0.181', 22) is listening
+                ** 5432
+                        ** Port ('0.0.0.0', 5432) not ready, starting...
+                ** 9876
+                        ** Port ('0.0.0.0', 9876) not ready, starting...
+                ** 4526
+                        ** Port ('0.0.0.0', 4526) not ready, starting...
+** Ready to work, run the client...
+** Received ping from ('192.168.0.181', 45980), sending pong...
+** Received ping from ('192.168.0.181', 45546), sending pong...
+** Received ping from ('192.168.0.181', 58778), sending pong...
+** Received ping from ('192.168.0.181', 45996), sending pong...
+** Received ping from ('192.168.0.181', 45562), sending pong...
+** Received ping from ('192.168.0.181', 58794), sending pong...
+
+## Output (Client)
+** Running port checks
+        ** Scanning host 192.168.0.1
+                ** Testing port 22      [OPENED]
+                ** Testing port 443     [CLOSED]
+        ** Scanning host 127.0.0.1
+                ** Testing port 22      [OPENED]
+                ** Testing port 5432    [CLOSED]
+                ** Testing port 443     [CLOSED]
+                ** Testing port 8443    [CLOSED]
---
+++ check_fw_py2.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python2
+##############################
+# Author: Ryan McGuire
+# Description: Server/Client firewall checker
+# Version: 20211014.01
+##############################
+import argparse
+import yaml
+import sys
+from lib.termcolor import colored
+import socket
+
+class firewall_tool:
+    def __init__(self, config_file, verbose = False):
+        self.config = dict()
+        self.verbose = verbose
+        self.listeners = list()
+        self.load_config(config_file)
+
+    def load_config(self, config_file):
+        with open(config_file, 'r') as config_file:
+            self.config = yaml.safe_load(config_file)
+
+    def run_checks(self):
+        if 'client' in self.config.keys() and 'targets' in self.config['client'].keys():
+            self.print_log('Running port checks',indent=0,bold=True)
+            if len(self.config['client']['targets']) > 0:
+                for target in self.config['client']['targets']:
+                    self.print_log('Scanning host '+target,bold=True)
+                    for port in self.config['client']['targets'][target]['ports']:
+                        self.print_log('Testing port '+str(port),indent=2,newline=False)
+                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        result = sock.connect_ex(( target, port ))
+                        if result:
+                            self.print_log('[CLOSED]', no_pre=True, sev='err')
+                        else:
+                            self.print_log('[OPENED]', no_pre=True, sev='good')
+
+            else:
+                self.print_log('Client defined with no targets, nothing to do, just gonna die',sev='err',bold=True)
+        else:
+            self.print_log('No client declared, nothing to do, just gonna die',sev='err',indent=0,bold=True)
+
+    def prepare_server(self):
+        if 'server' in self.config.keys():
+            self.print_log('Found server declaration, running ports...',indent=0,bold=True)
+            if 'ports' in self.config['server'].keys() and len(self.config['server']['ports']) > 0:
+                self.print_log('Port declarations found, ensuring listeners on ports:')
+                for port in self.config['server']['ports']:
+                    self.print_log(str(port),indent=2)
+                    self.server_port(port)
+                return True
+            else:
+                self.print_log('No ports declared for server, abandoning server mode',sev='warn')
+                return False
+        else:
+            self.print_log('No servers declared, skipping server mode',indent=0,bold=True,sev='warn')
+            return False
+
+    def server_port(self, port):
+        if len(str(port).split(':')) == 2:
+            addr = ( str(port).split(':')[0], int(str(port).split(':')[1]) )
+        else:
+            addr = ( '0.0.0.0', int(port) )
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if not sock.connect_ex(addr):
+            self.print_log('Port {} is listening'.format(str(addr)),sev='good',indent=3)
+        else:
+            self.print_log('Port {} not ready, starting...'.format(str(addr)),indent=3)
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.bind(addr)
+            server_socket.listen(10)
+            self.listeners.append(server_socket)
+
+    def start_server(self):
+        if self.prepare_server():
+            self.print_log('Ready to work, run the client...',indent=0,bold=True,sev='good')
+            while True:
+                for ss in self.listeners:
+                    cs, addr = ss.accept()
+                    data = cs.recv(1024)
+                    self.print_log('Received ping from {}, sending pong...'.format(addr),indent=0,sev='good')
+                    cs.send(str.encode('PONG'))
+                    cs.close()
+        else:
+            self.print_log('No server stuff to do, just gonna die now')
+
+    def print_log(self,msg,indent=1,sev='info',bold=False,no_pre=False,dt=False,newline=True):
+		# Log Prefix
+        if not no_pre:
+            if sev == 'info':
+                pre = colored('** ','cyan',attrs=['bold'])
+            elif sev == 'good':
+                pre = colored('** ','green',attrs=['bold'])
+            elif sev == 'warn':
+                pre = colored('*! ','yellow',attrs=['bold'])
+            elif sev == 'err':
+                pre = colored('!! !! ','red',attrs=['bold'])
+            else:
+                pre = colored('** ','cyan',attrs=['bold'])
+        else:
+            pre = ''
+		# Timestamp
+        if dt:
+            pre = colored(datetime.now(),"white","on_grey",attrs=["bold"]) + pre
+		# Msg Style
+        style = []
+        if bold:
+            style = ['bold']
+        if no_pre:
+            if sev == 'info':
+                msg = colored(msg,'cyan',attrs=['bold'])
+            elif sev == 'good':
+                msg = colored(msg,'green',attrs=['bold'])
+            elif sev == 'warn':
+                msg = colored(msg,'yellow',attrs=['bold'])
+            elif sev == 'err':
+                msg = colored(msg,'red',attrs=['bold'])
+            else:
+                msg = colored(msg,'cyan',attrs=['bold'])
+        msg = '\t'*indent + pre + colored(msg,attrs=style)
+        if self.verbose:
+            if newline:
+                print msg
+                sys.stdout.flush()
+            else:
+                print msg,
+                sys.stdout.flush()
+
+def main():
+    parser = argparse.ArgumentParser(description='Client/Server Firewall Test Tool')
+    parser.add_argument('-f', '--config', help='Config file location', required=True)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Be Verbose')
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument('-c', '--client', action='store_true', help='Client Mode')
+    mode.add_argument('-s', '--server', action='store_true', help='Server Mode')
+    args = parser.parse_args()
+    # Start up
+    fwc = firewall_tool(args.config, args.verbose)
+
+    if args.server:
+        fwc.start_server()
+    elif args.client:
+        fwc.run_checks()
+    else:
+        print('No work to do, just gonna die now')
+
+if __name__ == '__main__':
+    main()
